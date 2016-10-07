046f693eb9f05682f8136d2a3844e45a75780478
Ohad Zohar <ohad@Ohad-Zohars-MacBook-Pro.local>
2016-10-07 18:23:50 +0300

    added basic pokemon data hook

--- src/sdl/pokemon.cpp
+++ src/sdl/pokemon.cpp
@@ -11,6 +11,17 @@ static const char * MESSAGE_FORMAT = "POKEMSG %s\n";
 static const u16 POKEDEX_START  = 0xdbe4; 
 static const u16 POKEDEX_END  = 0xdc23; 
 static const u16 POKEDEX_LEN = POKEDEX_END - POKEDEX_START + 1;
+static const u16 PARTY_START = 0xda22;
+static const u16 PARTY_END = 0xdb49;
+static const u16 PARTY_NAMES_START = 0xdb4a;
+static const u16 PARTY_NAMES_END = 0xdbcd;
+static const u16 CURRENT_BOX_NUMBER = 0xD8BC; 
+static const u16 CURRENT_BOX_START = 0xad6c; // sram 1
+static const u16 CURRENT_BOX_END = 0xb1b9;
+static const u16 BOXES_START = 0xa000; //sram 2
+static const u16 BOXES_END = 0xbe2e;
+
+u8 current_sram_bank = 0;
 
 
 void message_js(char * msg)
@@ -90,22 +101,46 @@ void run_memory_hooks(u16 address, u8 value)
 {
     pokedex_hook(address);
     sram_hook(address, value);
+    box_and_party_hook(address);
 }
 
-
+extern u8 * gbRam;
+// bank x is in gbRam[x << 13]
 //ox4000 selects bank, 0xa to 0 enables, 0 to 0 disables
 void sram_hook(u16 address, u8 value)
 {
-    static u8 last_bank = 0;
     if (address == 0x4000)
     {
-        u8 bank = value;
-        if ((last_bank == 1 || last_bank == 2) && bank != 1 && bank != 2)
-        {
-            fprintf(stderr, "wrote boxes\n");
-        }
-        last_bank = bank;
+        current_sram_bank = value;
     }
+}
+
+void box_and_party_hook(u16 address)
+{
+    //TODO: current implementation results in 40 prints, find a good way to cache it.
+    static bool last_write_was_pokemon = false;
+    bool writing_pokemon = false;
+    if (address >= PARTY_START && address <= PARTY_END)
+    {
+        writing_pokemon  = true;
+    }
+    if (address == CURRENT_BOX_NUMBER)
+    {
+        writing_pokemon  = true;
+    }
+    if (current_sram_bank == 1 && address >= CURRENT_BOX_START && address <= CURRENT_BOX_END)
+    {
+        writing_pokemon  = true;
+    }
+    if (current_sram_bank == 2 && address >= BOXES_START && address <= BOXES_END)
+    {
+        writing_pokemon  = true;
+    }
+    if (!writing_pokemon && last_write_was_pokemon)
+    {
+        fprintf(stderr, "writing pokemon data\n");
+    }
+    last_write_was_pokemon = writing_pokemon;
 }
 
 void pokedex_hook(u16 address)
--- src/sdl/pokemon.h
+++ src/sdl/pokemon.h
@@ -4,5 +4,6 @@
 void message_js(char * msg);
 void handle_incoming_js_messages();
 void pokedex_hook(u16 address);
+void box_and_party_hook(u16 address);
 void run_memory_hooks(u16 address, u8 value);
 void sram_hook(u16 address, u8 value);
